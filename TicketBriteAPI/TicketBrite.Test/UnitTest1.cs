618a47866591255b0ff050e53a62582cad8578c7
gielvgorp <gielvgorp123@gmail.com>
2024-09-26 12:14:36 +0200

    init

---
+++ TicketBriteAPI/TicketBrite.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicketBrite.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
