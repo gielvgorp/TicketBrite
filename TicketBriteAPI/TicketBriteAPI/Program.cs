95996dae821c43b5c88479fac7be41b6798fdaa4
gielvgorp <gielvgorp123@gmail.com>
2024-12-13 11:31:07 +0100

    Created database seeder

--- TicketBriteAPI/TicketBriteAPI/Program.cs
+++ TicketBriteAPI/TicketBriteAPI/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using TicketBriteAPI.Hubs;
+using TicketBriteAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -72,7 +73,8 @@ using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     dbContext.Database.EnsureCreated();
-  
+
+    DatabaseSeeder.SeedDatabase(dbContext);
 }
 
 
