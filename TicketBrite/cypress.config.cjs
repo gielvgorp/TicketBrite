d9e8cbfe7fc1955d738182a1a66dd4a605fa835d
gielvgorp <gielvgorp123@gmail.com>
2025-01-12 18:50:02 +0100

    quality fixes and deleted screenshots and videos of e2e tests

--- TicketBrite/cypress.config.cjs
+++ TicketBrite/cypress.config.cjs
@@ -1,8 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  video: true,
-  screenshotOnRunFailure: true,
   e2e: {
     baseUrl: "http://localhost:5173",
   },
